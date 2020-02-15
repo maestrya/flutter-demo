139d809602e094734323770d58463e927640056d
velrino <denismagalhaesvelrino@gmail.com>
2020-02-15 18:13:53 -0300

    :zap: changed to package maestrya

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:maestrya/maestrya.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'AutoFront',
+      title: 'Maestrya',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -54,34 +55,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return null;
   }
 
-  List<Widget> handleWidgets(list) {
-    List<Widget> childrenWidgets = [];
-    for (final item in list) {
-      final value = new MapStringDynamicInferface(item);
-      if (item['type'] == 'text') {
-        childrenWidgets
-            .add(TextWidget(params: value));
-      } else if (item['type'] == 'input') {
-        childrenWidgets
-            .add(InputWidgetState(params: value));
-      } else if (item['type'] == 'select') {
-        childrenWidgets.add(
-            DropdownButtonWidget(params: value));
-      }
-       else if (item['type'] == 'cards') {
-        childrenWidgets.add(
-            CardWidgetState(params: value));
-      }
-    }
-    return childrenWidgets;
-  }
-
   Widget createListView() {
     List<Widget> childrenWidgets = [];
     String titleScaffold = "";
 
     if (list != null) {
-      childrenWidgets = this.handleWidgets(list['data']['body']['render']);
+      childrenWidgets =
+          Maestrya().render(list['data']['body']['render']);
       titleScaffold = list['data']['header']['title']['text'];
     }
 
@@ -101,139 +81,5 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             )));
-  }
-}
-
-class TextWidget extends StatelessWidget {
-  final MapStringDynamicInferface params;
-
-  TextWidget({Key key, @required this.params}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
-        child: Text(this.params.data['value']['text']));
-  }
-}
-
-class MapStringDynamicInferface {
-  final Map<String, dynamic> data;
-
-  MapStringDynamicInferface(this.data);
-}
-
-class InputWidgetState extends StatelessWidget {
-  final MapStringDynamicInferface params;
-
-  // In the constructor, require a Person
-  InputWidgetState({Key key, @required this.params}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: params.data['value']['name'],
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(25.0),
-                borderSide: BorderSide(),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AutoInputInferface {
-  final String label;
-
-  AutoInputInferface(this.label);
-}
-
-class DropdownButtonWidget extends StatefulWidget {
-  final MapStringDynamicInferface params;
-
-  DropdownButtonWidget({Key key, @required this.params}) : super(key: key);
-
-  @override
-  DropdownButtonWidgetState createState() =>
-      DropdownButtonWidgetState(params: null);
-}
-
-class DropdownButtonWidgetState extends State<DropdownButtonWidget> {
-  String dropdownValue = 'One';
-  final MapStringDynamicInferface params;
-
-  // In the constructor, require a Person
-  DropdownButtonWidgetState({Key key, @required this.params});
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class CardWidgetState extends StatelessWidget {
-  final MapStringDynamicInferface params;
-
-  // In the constructor, require a Person
-  CardWidgetState({Key key, @required this.params}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final paramsStyle = this.params.data['value'];
-
-    final styles = {
-      "scrollDirection": (paramsStyle['type'] == "horizontal") ? Axis.horizontal : Axis.vertical,
-      "groupHeight": paramsStyle['style']['groupHeight'].toDouble(),
-      "cardwidth": paramsStyle['style']['groupHeight'].toDouble(),
-    };
-
-    return Container(
-        height: styles['groupHeight'],
-        child: ListView.builder(
-          scrollDirection: styles['scrollDirection'],
-          itemCount: paramsStyle['cards'].length,
-          itemBuilder: (BuildContext context, int i) => Card(
-            child: Container(
-              width: styles['cardwidth'],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(paramsStyle['cards'][i]['text']),
-                ],
-              ),
-            ),
-          ),
-        ));
   }
 }
